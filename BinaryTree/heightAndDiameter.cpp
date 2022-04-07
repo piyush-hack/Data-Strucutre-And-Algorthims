5c23a198be93992fc0b76a1c8b55fd2eb361ab04
Piyush Puniya <75742731+piyush-hack@users.noreply.github.com>
2022-04-07 17:53:22 +0530

    updates upto binary tree

---
+++ BinaryTree/heightAndDiameter.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int treeHeight(Node *root)
+{
+
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int lheight = treeHeight(root->left);
+    int rheight = treeHeight(root->right);
+    return max(lheight, rheight) + 1;
+}
+
+int treeDiameter(Node *root)
+{
+
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int lheight = treeHeight(root->left);
+    int rheight = treeHeight(root->right);
+    int curDiameter = lheight + rheight + 1;
+    int lDia = treeDiameter(root->left);
+    int rDia = treeDiameter(root->right);
+
+    return curDiameter ;
+    // return max(curDiameter, max(lDia, rDia));
+}
+
+int main()
+{
+
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->left->left = new Node(8);
+    root->left->right->right = new Node(9);    
+    root->left->left->left->left = new Node(10);
+    root->left->right->right->right = new Node(11);
+
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout << treeHeight(root) << endl;
+    cout << treeDiameter(root) << endl;
+
+    return 0;
+}
